c10f1b3e91768ea5e78f5a47637d61351e796a36
maricha6 <maricha6@ncsu.edu>
2022-10-07 12:34:01 -0400

    [Initial Commit] basic package structure

---
+++ RTSGame/src/main/MainGameLoop.java
@@ -0,0 +1,12 @@
+package main;
+
+import renderEngine.Window;
+
+public class MainGameLoop {
+	
+    public static void main(String args[]) {
+    	Window window = Window.get();
+    	window.run();
+    }	
+
+}
